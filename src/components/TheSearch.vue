77331ca36a23ab6b3eb2db2fc922d5eb1f9b4fe5
Jack Huynh <kietdien88@gmail.com>
2024-05-21 14:50:24 +0700

    feat: Searching crypto token

--- src/components/TheSearch.vue
+++ src/components/TheSearch.vue
@@ -15,7 +15,7 @@ const { modelValue } = defineModels<{
       </div>
       <input
         v-bind="$attrs"
-        id="default-search" v-model="modelValue" type="search" class="block w-full border border-gray-300 rounded-lg bg-gray-50 p-4 ps-10 text-sm text-gray-900 dark:border-gray-600 focus:border-blue-500 dark:bg-gray-700 dark:text-white focus:ring-blue-500 dark:focus:border-blue-500 dark:focus:ring-blue-500 dark:placeholder-gray-400" placeholder="Search Mockups, Logos..." required
+        id="default-search" v-model="modelValue" type="search" class="block w-full border border-gray-300 rounded-lg bg-gray-50 p-4 ps-10 text-sm text-gray-900 dark:border-gray-600 focus:border-blue-500 dark:bg-gray-700 dark:text-white focus:ring-blue-500 dark:focus:border-blue-500 dark:focus:ring-blue-500 dark:placeholder-gray-400" placeholder="Search Symbol, Name..." required
       >
       <button type="submit" class="absolute end-2.5 bottom-2.5 rounded-lg bg-blue-700 px-4 py-2 text-sm text-white font-medium dark:bg-blue-600 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         Search
